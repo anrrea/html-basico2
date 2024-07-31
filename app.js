b5f9bf9fa5aa9bc18acb28613b64da888fc3314c
anrrea <rolonandrea@gmail.com>
2024-07-31 11:17:23 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+//esto es un comentario
+
+
+    /* habia una vez tres cerditos,
+    que estaban programando un codiguito
+    */
+   console.log("Holiss");
+   // crear variables
+ let universidad = "utiki"; //string
+ //funcion typeof muestra el tipo de dato
+ console.log(typeof(universidad));
+ let edad = 45;//
+ console.log(typeof(edad));
+ debugger;
+ let pi = 3.14;
+ console.log(typeof(pi))
+ let soyMillonario = false;
+ console.log(typeof(soyMillonario));
+
+ // arreglo o vector o matriz
+ let arreglo = [
+     universidad, edad
+     , soyMillonario
+ ];
+ const noBasta = "Si basta, el vasto, con el pasto";
+ //noBasta "778766";
+ console.log(arreglo);
+ //length, muestra cantidad o elemento de un arreglo
+ console.log(arreglo.length);
+
+ //recorridos
+ for(let i=0; i<arreglo.length; i++) {
+    debugger;//para la ejecucio en esta linea
+    console.log(arreglo[i]);
+ }
+
+
